d55b8db5bf2d1a106f81ee6b39375390ac67e313
Brian Cheptiony <cheptiony6@gmail.com>
2025-06-27 12:24:10 +0300

    initial

--- server/testing/models_test.py
+++ server/testing/models_test.py
@@ -1,32 +1,48 @@
+import pytest
 from datetime import datetime
+from server.app import app, db
+from server.models import Message
 
-from app import app
-from models import db, Message
-
-class TestMessage:
-    '''Message model in models.py'''
+@pytest.fixture(scope='module')
+def test_client():
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     with app.app_context():
-        m = Message.query.filter(
-            Message.body == "Hello 👋"
-            ).filter(Message.username == "Liza")
+        db.create_all()
+        yield app.test_client()
+        db.session.remove()
+        db.drop_all()
 
-        for message in m:
-            db.session.delete(message)
+class TestMessage:
+    '''Tests for the Message model'''
 
-        db.session.commit()
-
-    def test_has_correct_columns(self):
-        '''has columns for message body, username, and creation time.'''
+    def test_has_correct_columns(self, test_client):
         with app.app_context():
-
             hello_from_liza = Message(
                 body="Hello 👋",
-                username="Liza")
-            
+                username="Liza"
+            )
             db.session.add(hello_from_liza)
             db.session.commit()
 
-            assert(hello_from_liza.body == "Hello 👋")
-            assert(hello_from_liza.username == "Liza")
-            assert(type(hello_from_liza.created_at) == datetime)
+            assert hello_from_liza.body == "Hello 👋"
+            assert hello_from_liza.username == "Liza"
+            assert isinstance(hello_from_liza.created_at, datetime)
+
+            db.session.delete(hello_from_liza)
+            db.session.commit()
+
+    def test_creates_message(self, test_client):
+        with app.app_context():
+            message = Message(body="Hello 👋", username="Liza")
+            db.session.add(message)
+            db.session.commit()
+
+            saved_message = Message.query.filter_by(body="Hello 👋").first()
+            assert saved_message is not None
+            assert saved_message.username == "Liza"
+
+            db.session.delete(saved_message)
+            db.session.commit()
